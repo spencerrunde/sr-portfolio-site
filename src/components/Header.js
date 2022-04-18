db1cd522fa6f11181c1c527819af1a8e0def45dc
spencerrunde <spencerrunde@gmail.com>
2022-04-18 01:29:25 -0700

    Updating resume file

--- src/components/Header.js
+++ src/components/Header.js
@@ -69,7 +69,7 @@ function Header({ VscMenu, VscClose }) {
               <em>03</em> contact
             </a>
             <a
-              href="/assets/spencer_runde_resume.pdf"
+              href="/assets/spencerRundeResume.pdf"
               target="_blank"
               rel="noopener noreferrer"
               onClick={handleNavbar}
